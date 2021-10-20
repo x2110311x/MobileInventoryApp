0dbc7bc5868551cff8da372cdc255b2c755abb90
Alex Sweeney <x2110311x@gmail.com>
2021-10-19 22:40:05 -0400

    Create basic OAUTH login

---
+++ api/calls/login.js
@@ -0,0 +1,53 @@
+const msal = require('@azure/msal-node');
+const config = require('../config.json');
+const SERVER_PORT = config.SERVER_PORT;
+const REDIRECT_URI = config.REDIRECT_URI;
+
+const pconfig = {
+    auth: {
+        clientId: config.auth.clientId,
+        authority: config.auth.authority,
+        clientSecret: config.auth.clientSecret
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: msal.LogLevel.Verbose,
+        }
+    }
+};
+
+const pca = new msal.ConfidentialClientApplication(pconfig);
+
+module.exports =
+function test(app) {
+    app.get('/auth-redirect', (req, res) => {
+        const tokenRequest = {
+            code: req.query.code,
+            scopes: ["user.read"],
+            redirectUri: REDIRECT_URI,
+        };
+    
+        pca.acquireTokenByCode(tokenRequest).then((response) => {
+            console.log("\nResponse: \n:", response);
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log(error);
+            res.status(500).send(error);
+        });
+    });
+    app.get('/login', (req, res) => {
+        const authCodeUrlParameters = {
+            scopes: ["user.read"],
+            redirectUri: REDIRECT_URI,
+        };
+    
+        // get url to sign user in and consent to scopes needed for application
+        pca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
+            res.redirect(response);
+        }).catch((error) => console.log(JSON.stringify(error)));
+    });
+}

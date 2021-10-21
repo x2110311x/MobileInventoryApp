b0663c40310e90406fceb8f5902403ebe8341185
Alex Sweeney <x2110311x@gmail.com>
2021-10-20 23:55:39 -0400

    Write Stored Procedures to Add Data

---
+++ sql/ProcedureAdmin Permissions.sql
@@ -0,0 +1,5 @@
+﻿CREATE USER 'procadmin'@'localhost' IDENTIFIED BY <password>
+GRANT SELECT, INSERT, UPDATE, DELETE on inventoryapp.* to 'procadmin'@'localhost';
+GRANT ALTER ROUTINE, CREATE ROUTINE, EXECUTE ON *.* to 'procadmin'@'localhost';
+ALTER USER 'procadmin'@'localhost' ACCOUNT LOCK;
+FLUSH PRIVILEGES;

db04e02c6e1b58b877a52673331ecd7541537577
Alex Sweeney <x2110311x@gmail.com>
2021-11-04 23:51:34 -0400

    Add auth middleware and test query path

--- api/api/index.js
+++ api/api/index.js
@@ -1,13 +1,37 @@
-var express = require('express')
-var router = express.Router();
-var passport = require('passport');
-
+const express = require('express')
+const router = express.Router();
+const passport = require('passport');
+const fetch = require('node-fetch');
+const config = require("../config.json");
+const db = require('../helpers/db');
 router.use(passport.authenticate("oauth-bearer", { session: false }));
+
+router.use(
+    function setUser(req, res, next){
+        fetch('https://graph.microsoft.com/v1.0/me', {
+        method: 'get',
+        headers: { 'authorization': `Bearer ${req.header('X-Auth')}` },
+        })
+        .then(response => response.json())
+        .then(json => req.uid = json.userPrincipalName.split('@')[0])
+        .then(() => next());
+    }
+);
 
 require('./companies')(router);
 require('./items')(router);
 require('./orders')(router);
 require('./vendors')(router);
+
+router.get('/test', (req, res) => {
+    let pass = req.header('X-Auth');
+    db(req.uid, pass, "SELECT * FROM vendors")
+    .then((rows) =>{
+        res.json(rows);
+    }).catch((err)=> {
+        res.status(500).send(err);
+    });
+});
 
 router.get('*', function (req, res) {
     res.status(403).send("Unknown request");

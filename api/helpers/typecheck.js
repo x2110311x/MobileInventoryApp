371e680dd37bbefccd78b2b450480a3f2504ba63
Alex Sweeney <x2110311x@gmail.com>
2021-11-09 15:42:14 -0500

    Created typecheck helper to test input types

---
+++ api/helpers/typecheck.js
@@ -0,0 +1,31 @@
+/*jshint esversion: 6 */
+module.exports = {
+	checkFloat: function(num){
+		if(!num) return;
+		let retVar = parseFloat(num);
+		if(isNaN(retVar)){
+			return;
+		} else {
+			return retVar;
+		}
+	},
+	checkInt: function(num){
+		if(!num) return;
+		let retVar = parseFloat(num);
+		if(isNaN(retVar)){
+			return;
+		} else {
+			return retVar;
+		}
+	},
+	checkBool: function(str){
+		if(!str) return;
+		if(str.toString().toLowerCase() == 'true'){
+			return true;
+		} else if (str.toString().toLowerCase() == 'false') {
+			return false;
+		} else {
+			return;
+		}
+	}
+};

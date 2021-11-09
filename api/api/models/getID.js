4120032df4cd088fbafc11d8a40e310e8bd725d7
Alex Sweeney <x2110311x@gmail.com>
2021-11-08 20:09:36 -0500

    Run ESLint on models/getID.js

--- api/api/models/getID.js
+++ api/api/models/getID.js
@@ -1,17 +1,17 @@
-const db = require("../../helpers/db");
+const db = require('../../helpers/db');
 
 module.exports =
 function models_getID(app) {
-    app.get('/models/:modelid', (req, res) => {
-        let pass = req.header('X-Auth');
-        db(req.uid, pass, `SELECT * FROM models WHERE id = ${req.params.modelid}`)
-        .then((rows) =>{
-            let row = rows[0];
-            row.url = `/models/${row.id}`;
-            res.json(row);
-        }).catch((err)=> {
-            console.error(err);
-            res.status(500).send("Server Error");
-        });
-    });
-}
+	app.get('/models/:modelid', (req, res) => {
+		let pass = req.header('X-Auth');
+		db(req.uid, pass, `SELECT * FROM models WHERE id = ${req.params.modelid}`)
+			.then((rows) =>{
+				let row = rows[0];
+				row.url = `/models/${row.id}`;
+				res.json(row);
+			}).catch((err)=> {
+				console.error(err);
+				res.status(500).send('Server Error');
+			});
+	});
+};

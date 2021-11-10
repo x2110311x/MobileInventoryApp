4ab7102b5343efcbe82b072ab472fd51ff10199c
Alex Sweeney <x2110311x@gmail.com>
2021-11-10 01:41:37 +0000

    Change order of middleware

--- api/api/index.js
+++ api/api/index.js
@@ -5,8 +5,8 @@ const db = require('../helpers/db');
 
 const router = express.Router();
 
-router.use(require('../helpers/checkHeaders.js'));
 router.use(passport.authenticate('oauth-bearer', { session: false }));
+router.use(require('../helpers/checkHeaders.js'));
 router.use(require('../helpers/setUser.js'));
 
 require('./companies')(router);

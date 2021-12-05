08c301e8762b4a7219a433a5efd42c3e20af46c3
Alex Sweeney <x2110311x@gmail.com>
2021-12-05 13:23:29 -0500

    Change app callback

--- api/auth/applogin.js
+++ api/auth/applogin.js
@@ -24,6 +24,23 @@ const pca = new msal.ConfidentialClientApplication(pconfig);
 
 module.exports =
 function login(app) {
+	app.get('/appcallback', (req, res) => {
+		// #swagger.summary = 'Handle Azure AD OAUTH2.0 callback'
+		const tokenRequest = {
+			code: req.query.code,
+			scopes: ['user.read'],
+			redirectUri: REDIRECT_URI,
+		};
+    
+		pca.acquireTokenByCode(tokenRequest).then((response) => {
+			console.log('\nResponse: \n:', response);
+			let url = `${config.auth.appcallbackURL}?access=${response.accessToken}&id=${response.idToken}`;
+			res.redirect(url);
+		}).catch((error) => {
+			console.log(error);
+			res.status(500).send(error);
+		});
+	});
 	app.get('/applogin', (req, res) => {
 		// #swagger.summary = 'Redirect to OAUTH2.0 sign in for Android
 		const authCodeUrlParameters = {

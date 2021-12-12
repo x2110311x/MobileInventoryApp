cb797e9c5268455beb9724582d5ed4acdefaf426
Alex Sweeney <x2110311x@gmail.com>
2021-12-12 13:41:07 -0500

    Do not store any login details on rooted devices

--- app/app/src/main/java/com/asweeney/inventoryapp/LoginCallback.kt
+++ app/app/src/main/java/com/asweeney/inventoryapp/LoginCallback.kt
@@ -1,10 +1,12 @@
 package com.asweeney.inventoryapp
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 
 class LoginCallback : AppCompatActivity() {
@@ -12,20 +14,37 @@ class LoginCallback : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_callback)
         val data: Uri? = intent?.data
-        val params = data!!.toString().split("?access=").toTypedArray()[1].split("&id=").toTypedArray()
-        val accessToken = params[0]
-        val idToken = params[1].replace("#","")
-        val sharedPref = getSharedPreferences("com.asweeney.inventory.LOGIN", MODE_PRIVATE)
-        with (sharedPref.edit()) {
-            putString("access_token", accessToken)
-            putString("id_token", idToken)
-            apply()
+        if (RootDetection.check(applicationContext)) {
+            forceCloseApp(this)
+        } else {
+            val params =
+                data!!.toString().split("?access=").toTypedArray()[1].split("&id=").toTypedArray()
+            val accessToken = params[0]
+            val idToken = params[1].replace("#", "")
+            val sharedPref = getSharedPreferences("com.asweeney.inventory.LOGIN", MODE_PRIVATE)
+            with(sharedPref.edit()) {
+                putString("access_token", accessToken)
+                putString("id_token", idToken)
+                apply()
+            }
+            Toast.makeText(applicationContext, "Login Successful", Toast.LENGTH_SHORT).show()
+            startActivity(
+                Intent(this, MainPage::class.java)
+                    .putExtra("Source", "from LoginCallback")
+            )
+            finish()
         }
-        Toast.makeText(applicationContext, "Login Successful", Toast.LENGTH_SHORT).show()
-        startActivity(
-            Intent(this, MainPage::class.java)
-            .putExtra("Source", "from LoginCallback"))
-        finish()
+    }
+    private fun forceCloseApp(ctx: Context) {
+        val alert = AlertDialog.Builder(ctx)
+            .setMessage("This application cannot be run on rooted devices")
+            .setCancelable(false)
+            .setPositiveButton("Proceed") { _, _ ->
+                finish()
+            }
+            .create()
+        alert.setTitle("Root detected")
+        alert.show()
     }
 
 }

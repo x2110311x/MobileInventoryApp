d77c9cac158fdd487402a26dd2559fbd0ef6d026
Alex Sweeney <x2110311x@gmail.com>
2021-12-12 07:28:44 +0000

    setup http redirects

--- api/index.js
+++ api/index.js
@@ -10,6 +10,15 @@ const fs = require('fs');
 var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
 var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 var app = express();
+var httpserv = express();
+httpserv.use((req, res) => {
+	if (req.headers.host === 'x2110311x.ddns.net')
+		return res.redirect(301, 'https://x2110311x.ddns.net:4443');
+	if (req.headers['x-forwarded-proto'] !== 'https')
+		return res.redirect('https://' + 'x2110311x.ddns.net:4443' + req.url) ;
+	else
+		return res.sendStatus(405);
+});
 
 const config = require('./config.json');
   
@@ -19,9 +28,10 @@ var bearerStrategy = new BearerStrategy(config.passport,
 	}
 );
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 app.use(express.json());
-app.use(require('express-session')({ secret: config.sessionsecret, resave: true, saveUninitialized: true, secure: true }));
+app.use(require('express-session')({ secret: config.sessionsecret, resave: true,
+	saveUninitialized: true, secure: true, httpOnly: true, sameSite: true}));
 app.use(passport.initialize());
 passport.use(bearerStrategy);
 
@@ -46,7 +56,7 @@ app.all('*', function (req, res) {
 
 
 var credentials = {key: privateKey, cert: certificate};
-var httpServer = http.createServer(app);
+var httpServer = http.createServer(httpserv);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(config.SERVER_PORT);
